ae69a96bb7d0b604c7b74cf1fb2cb11150bd3245
Lucas Gomes <lucasgomesp22@gmail.com>
2016-10-23 21:10:34 -0200

    Criação e Estruturação WebAPI

---
+++ NgTodoList/NgTodoList.Api/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using NgTodoList.Domain;
+using NgTodoList.Domain.Repositories;
+using System.Web.Http;
+
+namespace NgTodoList.Api.Controllers
+{
+    public class UserController : ApiController
+    {
+        private IUserRepository _repository;
+
+        public UserController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public User Get(string email)
+        {
+            return _repository.Get(email);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _repository.Dispose();
+        }
+    }
+}
---
+++ NgTodoList/NgTodoList.Utils/Helpers/LogErrorHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgTodoList.Utils.Helpers
+{
+    public static class LogErrorHelper
+    {
+        public static void Register(Exception ex)
+        {
+
+        }
+    }
+}
